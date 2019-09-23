78f95d0c95f2d5ade286e6e2f790d9159cffd392
Chris Young <beesucker@gmail.com>
2019-09-23 13:01:45 -0700

    modelize everything, not just roots

--- lib/hecks/active_model.rb
+++ lib/hecks/active_model.rb
@@ -3,20 +3,21 @@ require 'hecks-app'
 module Hecks
   module ActiveModel
     def self.decorate
-      HecksApp::DOMAIN_SCHEMA.aggregates.each do |aggregate|
-        root_name = aggregate.instance_variable_get(:@root).name
+      HecksApp::DOMAIN_SCHEMA.aggregates.each do |aggregate_from_schema|
         HecksApp::ApplicationPort
           .domain::Domain
-          .const_get(aggregate.name)
-          .const_get(root_name).tap do |root|
-            root.include(::ActiveModel::Model)
-
-            root.define_singleton_method :model_name do
-              ::ActiveModel::Name.new(
-                self,
-                nil,
-                "#{aggregate.name}::#{root_name}"
-              )
+          .const_get(aggregate_from_schema.name).tap do |aggregate|
+            aggregate.constants.each do |domain_model_name|
+              aggregate.const_get(domain_model_name).tap do |domain_model|
+                domain_model.include(::ActiveModel::Model)
+                domain_model.define_singleton_method :model_name do
+                  ::ActiveModel::Name.new(
+                    self,
+                    nil,
+                    domain_model.name.to_s.split('::')[-2..-1].join('::')
+                  )
+                end
+              end
             end
           end
       end
